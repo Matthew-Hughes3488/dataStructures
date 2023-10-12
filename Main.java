c4ab54ed2218a32f2ca87c3565e099818ba09860
Matthew Hughes <57641028+Matthew-Hughes3488@users.noreply.github.com>
2023-10-12 17:08:11 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,5 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        Stack stack = new Stack();
-        stack.push(0);
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        stack.push(4);
-        stack.push(5);
-        stack.push(6);
-        stack.push(3);
-        stack.push(3);
-        stack.push(3);
-
-        System.out.println(stack.pop());       
+        //Edit code here to mess around with the other classes
     }
 }
